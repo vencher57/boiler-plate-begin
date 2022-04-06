6dc6150aad3ce7cb9c0869e1255d1bb1da1c9a52
Seokho Moon <vencher57@naver.com>
2022-04-06 14:03:11 +0900

    비밀정보보호

--- index.js
+++ index.js
@@ -2,8 +2,17 @@ const express = require('express');
 const app = express();
 const port = 5000;
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
-mongoose.connect('mongodb+srv://moonseokho:fkrm5623@cluster0.l8v2x.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+const config = require('./config/key');
+const { User } = require('./Models/User');
+
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(bodyParser.json());
+
+mongoose.connect(config.mongoURI, {
     useNewUrlParser: true, useUnifiedTopology: true
 }).then(() => {
     console.log("MongoDB Connected...")
@@ -12,8 +21,23 @@ mongoose.connect('mongodb+srv://moonseokho:fkrm5623@cluster0.l8v2x.mongodb.net/m
 })
 
 
-
-
 app.get('/', (req, res) => res.send("Hello World!!"));
+
+app.post('/register', (req, res) => {
+    //회원 가입 할때 필요한 정보들을 client에서 가져오면
+    //데이터베이스에 인서트 해준다.
+    const user = new User(req.body);
+
+    user.save((err, userInfo) => {
+        if(err) {
+            return res.json({ success: false, err});
+        } else {
+            return res.status(200).json({
+                success: true
+            });
+        }
+    });
+
+})
 
 app.listen(port, () => console.log (`Example app listening on port ${port}!`));
